a81c4fa91abf8f38db6d3c84c1852f4a7be82029
RogerM <rogermao2019@gmail.com>
2024-08-28 20:13:34 -0400

    created composables for one affirmation, will create a list next time

--- app/src/main/java/com/example/a30days/MainActivity.kt
+++ app/src/main/java/com/example/a30days/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.a30days.data.ActivitiesDataSource
+import com.example.a30days.data.CoupleActivity
 import com.example.a30days.ui.theme._30DaysTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +35,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             _30DaysTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CoupleActivityCard(coupleActivity: CoupleActivity, dayNumber: Int) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+        ){
+            Text(
+                text = "Day #${dayNumber}: " + stringResource(id = coupleActivity.stringID),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Image(
+                painter = painterResource(id = coupleActivity.imageID),
+                contentDescription = stringResource(id = coupleActivity.stringID),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(200.dp)
+            )
+            Text(
+                text = stringResource(id = coupleActivity.descriptionID),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     _30DaysTheme {
-        Greeting("Android")
+        CoupleActivityCard(coupleActivity = ActivitiesDataSource.activitiesList[0], 1)
     }
 }
